b2afeef6f094d59ec2c83053c60d3385af903380
Zan Makarov <z.makarov@codat.io>
2022-12-30 14:58:59 +0000

    uploading initial files

---
+++ fetcher.js
@@ -0,0 +1,32 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+const fs_1 = __importDefault(require("fs"));
+const addreses = ["CIK0000320193"];
+// change the things inside the quotation marks to get data from another place
+const URL = "https://data.sec.gov/api/xbrl/companyconcept/CIK0000320193/us-gaap/AccountsPayableCurrent.json";
+addreses.forEach((address) => {
+    const URL = `https://data.sec.gov/api/xbrl/companyconcept/${address}/us-gaap/AccountsPayableCurrent.json`;
+    const res = axios_1.default.get(URL);
+    res.then((d) => {
+        const data = JSON.stringify(d.data);
+        fs_1.default.writeFile(`${address}-data.json`, data, (e) => {
+            console.log(e);
+        });
+        console.log(data);
+    });
+});
+// const data = (() => {
+//   const res = axios.get(URL);
+//   res.then((d) => {
+//     const data = JSON.stringify(d.data);
+//     fs.writeFile("data.json", data, (e) => {
+//       console.log(e);
+//     });
+//     console.log(data);
+//   });
+//   console.log("hi");
+// })();
---
+++ fetcher.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosResponse } from "axios";
+import z from "zod";
+import fs from "fs";
+
+const addreses = ["CIK0000320193"];
+// change the things inside the quotation marks to get data from another place
+const URL =
+  "https://data.sec.gov/api/xbrl/companyconcept/CIK0000320193/us-gaap/AccountsPayableCurrent.json";
+
+addreses.forEach((address) => {
+  const URL = `https://data.sec.gov/api/xbrl/companyconcept/${address}/us-gaap/AccountsPayableCurrent.json`;
+  const res = axios.get(URL);
+  res.then((d) => {
+    const data = JSON.stringify(d.data);
+    fs.writeFile(`${address}-data.json`, data, (e) => {
+      console.log(e);
+    });
+    console.log(data);
+  });
+});
+
+// const data = (() => {
+//   const res = axios.get(URL);
+//   res.then((d) => {
+//     const data = JSON.stringify(d.data);
+//     fs.writeFile("data1.json", data, (e) => {
+//       console.log(e);
+//     });
+//     console.log(data);
+//   });
+//   console.log("hi");
+// })();
